fbb6b0519f4547b0ec44b4d1e17712bb119d55bd
pb010-temp <pb@example.com>
2026-09-20 18:51:09 +0530

    Initial Node.js application for blue-green deployment

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/status', (req, res) => {
+    res.json({
+        status: 'API is running - Jenkins CI/CD'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
